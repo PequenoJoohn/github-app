211428520e419953792170dfeba1b4e7a5f06444
pequenojoohn <kheymk@hotmail.com>
2020-12-07 18:40:16 -0300

    Started.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          type="search"
+          placeholder="Digite o nome do usuário no github"
+        />
+      </div>
+      <div className="user-info">
+        <img src="https://avatars2.githubusercontent.com/u/25863252?v=4" alt="" />
+        <h1>
+          <a href="https://github.com/fdaciuk"> Jonathan Matos </a>
+        </h1>
+
+        <ul className="repos-info">
+          <li>Repositórios: 29</li>
+          <li>Seguidores: 35</li>
+          <li>Seguindo: 49</li>
+        </ul>
+
+        <div className="actions">
+          <button>Ver repositórios</button>
+          <button>Ver favoritos</button>
+        </div>
+
+        <div className="repos">
+          <h2>Repositórios:</h2>
+          <ul>
+            <li><a href="#">Nome do repositório</a></li>
+          </ul>
+        </div>
+
+        <div className="starred">
+          <h2>Favoritos:</h2>
+          <ul>
+            <li><a href="#">Nome do repositório</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
