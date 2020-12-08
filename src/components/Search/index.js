63e73785d4e64db1a4e540926ac910847db3cb9f
pequenojoohn <kheymk@hotmail.com>
2020-12-08 12:39:46 -0300

    Search using fetch get user.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -4,6 +4,18 @@ const Search = () => {
             <input
                 type="search"
                 placeholder="Digite o nome do usuário no github"
+                onKeyUp={(e) => {
+                    const { value } = e.target
+                    const keyCode = e.which || e.keyCode
+                    const ENTER = 13
+                    if (keyCode === ENTER) {
+                        fetch(`https://api.github.com/users/${value}`, { method: "GET" })
+                            .then(response => response.json())
+                            .then((data) => {
+                                console.log(data);
+                            }).catch(error => console.log(error));
+                    }
+                }}
             />
         </div>
     )
